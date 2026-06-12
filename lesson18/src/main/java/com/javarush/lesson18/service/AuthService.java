3e430291106e1df014bbd12f21d177e1092da3ba
akhmelev <akhmelev@gmail.com>
2026-06-12 09:56:37 +0300

    lesson18 Security with DB

---
+++ lesson18/src/main/java/com/javarush/lesson18/service/AuthService.java
@@ -0,0 +1,25 @@
+package com.javarush.lesson18.service;
+
+import org.springframework.security.core.userdetails.User;
+import com.javarush.lesson18.repository.Repo;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AuthService implements UserDetailsService {
+
+    private final Repo repo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var inDb = repo.findByLogin(username);
+        return User.withUsername(inDb.getLogin())
+                .password(inDb.getPassword())
+                .roles(inDb.getRole().getRole())
+                .build();
+    }
+}
