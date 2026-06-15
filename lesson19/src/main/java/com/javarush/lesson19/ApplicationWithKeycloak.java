d41e8b86035cfb1c5f98ec1e446a455c6004edc8
akhmelev <akhmelev@gmail.com>
2026-06-15 21:03:07 +0300

    lesson19 Keycloak + github oauth2

---
+++ lesson19/src/main/java/com/javarush/lesson19/ApplicationWithKeycloak.java
@@ -0,0 +1,14 @@
+package com.javarush.lesson19;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication//(exclude = { SecurityAutoConfiguration.class })
+public class ApplicationWithKeycloak {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationWithKeycloak.class, args);
+        log.info("Application started http://localhost:8080");
+    }
+}
