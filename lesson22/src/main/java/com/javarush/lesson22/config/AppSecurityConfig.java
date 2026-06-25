bd14a5bf73256cb974143a8bc3688ea0f31b5d41
akhmelev <akhmelev@gmail.com>
2026-06-25 22:31:05 +0300

    lesson22 Playwright

---
+++ lesson22/src/main/java/com/javarush/lesson22/config/AppSecurityConfig.java
@@ -0,0 +1,55 @@
+package com.javarush.lesson22.config;
+
+import com.javarush.lesson22.entity.Role;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class AppSecurityConfig {
+
+    @Bean
+    @Order(SecurityProperties.BASIC_AUTH_ORDER)
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable) //see userpage.html
+                .authorizeHttpRequests(requests -> requests
+                        //
+                        .requestMatchers("/login", "/logout", "/signup")
+                        .permitAll()
+                        //
+                        .requestMatchers(HttpMethod.GET,"/users")
+                        .hasAnyAuthority(Role.USER.getAuthority(), Role.ADMIN.getAuthority())
+                        //
+                        .requestMatchers(HttpMethod.POST,"/users")
+                        .hasAuthority(Role.ADMIN.getAuthority())
+                        //
+                        .anyRequest()
+                        .authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .usernameParameter("login")
+                        .passwordParameter("password")
+                        .defaultSuccessUrl("/users")
+                        .failureUrl("/login?error")
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        PasswordEncoder delegatingPasswordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return delegatingPasswordEncoder;
+    }
+}
