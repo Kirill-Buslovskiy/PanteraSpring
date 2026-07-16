1dbe836281118427604073987822c108a032c59a
akhmelev <akhmelev@gmail.com>
2026-07-16 20:40:39 +0300

    lesson19 reg in eureka

--- lesson19/src/main/java/com/javarush/lesson19/ApplicationWithKeycloak.java
+++ lesson19/src/main/java/com/javarush/lesson19/ApplicationWithKeycloak.java
@@ -3,9 +3,11 @@ package com.javarush.lesson19;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @Slf4j
 @SpringBootApplication//(exclude = { SecurityAutoConfiguration.class })
+@EnableDiscoveryClient
 public class ApplicationWithKeycloak {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationWithKeycloak.class, args);
